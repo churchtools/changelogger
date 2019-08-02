830c8eb1de392cc2d086e4db4054b8395a8aec5e
Hans-Helge Buerger <mail@hanshelgebuerger.de>
2019-08-02 14:04:51 +0200

    Test Clean Command

---
+++ tests/Feature/Commands/CleanTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature\Commands;
+
+use Illuminate\Support\Facades\File;
+use Tests\TestCase;
+
+/**
+ * Class CleanTest
+ * @package Tests\Feature\Commands
+ * @covers \App\Commands\Clean
+ */
+class CleanTest extends TestCase
+{
+
+    public function testAttemptToCleanNoChangesGivesInfo() : void
+    {
+        $this->assertDirectoryExists(config('changelogger.unreleased'));
+        $this->artisan('clean')
+            ->expectsOutput('No logs. Nothing to delete.')
+            ->assertExitCode(0);
+    }
+
+
+    public function testCleanOneFileAndExpectCorrectInfo(): void
+    {
+        $this->assertDirectoryExists(config('changelogger.unreleased'));
+        File::put(config('changelogger.unreleased') . '/test.yml', 'Test');
+        $this->artisan('clean')
+            ->expectsQuestion('Do you want to delete 1 file?', true)
+            ->assertExitCode(0);
+
+        $this->assertFileNotExists(config('changelogger.unreleased') . '/test.yml');
+    }
+}
